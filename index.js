79788db807f756ccef428fe5404c167a9ff451e7
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-09 11:19:12 -0400

    Refactor constructor with add & sort methods

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ function WordTries(words, depth) {
   }
 
   words.forEach((word, idx) => {
-    this.wordFrequencies[word] = this.wordFrequencies[word] || {
-      count: 0,
-      nextWords: {},
-    }
-    this.wordFrequencies[word].count += 1
+    this.add(word, false)
+
     if (idx + 1 < words.length) {
       const nextWords = this.wordFrequencies[word].nextWords
       nextWords[words[idx + 1]] = nextWords[words[idx + 1]] || 0
@@ -19,9 +16,7 @@ function WordTries(words, depth) {
     }
   })
 
-  this.words = Object.keys(this.wordFrequencies).sort((a, b) => {
-    return this.wordFrequencies[b].count - this.wordFrequencies[a].count
-  })
+  this.sort()
 
   Object.keys(this.wordFrequencies).forEach(word => {
     this.wordFrequencies[word].nextWords = new WordTries(
@@ -46,9 +41,23 @@ WordTries.prototype.get = function(...words) {
   }
 }
 
-// WordTries.prototype.add = function(word) {
-//   this.wordFrequencies[word] = this.wordFrequencies[word] || 0
-//   this.wordFrequencies[word] += 1
-// }
+WordTries.prototype.sort = function() {
+  this.words = Object.keys(this.wordFrequencies).sort((a, b) => {
+    return this.wordFrequencies[b].count - this.wordFrequencies[a].count
+  })
+}
+
+WordTries.prototype.add = function(word, sort = true) {
+  this.wordFrequencies[word] = this.wordFrequencies[word] || {
+    count: 0,
+    nextWords: {},
+  }
+
+  this.wordFrequencies[word].count += 1
+
+  if (sort) {
+    this.sort()
+  }
+}
 
 module.exports = WordTries
